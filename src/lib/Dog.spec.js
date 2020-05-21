031c45bf9a401c7cd9d6447f97be48feb2754dbc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-21 11:09:10 -0400

    start with Cypress

---
+++ src/lib/Dog.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+import React from 'react'
+import {mount} from 'cypress-react-unit-test'
+import Dog from './Dog'
+
+describe('Dog', () => {
+  it('barks', () => {
+
+  })
+})
